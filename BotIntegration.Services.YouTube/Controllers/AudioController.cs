e037b1fb4571c397f327b683a2658080ec2bd734
Nikita Efimov <maskedballmail@gmail.com>
2024-08-21 17:22:24 +0300

    added youtube api

---
+++ BotIntegration.Services.YouTube/Controllers/AudioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace BotIntegration.Services.YouTube.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AudioController(ILogger<AudioController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] string videoUrl)
+    {
+        if (string.IsNullOrEmpty(videoUrl))
+        {
+            logger.LogWarning("VideoUrl parameter is required.");
+            return BadRequest("VideoUrl parameter is required.");
+        }
+
+        try
+        {
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(videoUrl);
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+            var memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "audio/mp3", $"{video.Title}.mp3");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while processing the request: {VideoUrl}", videoUrl);
+            return StatusCode(500, "An internal server error occurred. Please try again later.");
+        }
+    }
+}
---
+++ BotIntegration.Services.YouTube/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BotIntegration.Services.YouTube.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController(ILogger<HealthCheckController> logger) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        logger.LogInformation("Health check endpoint was called.");
+        return Ok(new { status = "Healthy" });
+    }
+}
