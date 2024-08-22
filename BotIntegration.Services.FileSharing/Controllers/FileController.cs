149fb4bad03dfb1296b164d096be60150f8d1954
Nikita Efimov <maskedballmail@gmail.com>
2024-08-22 13:18:54 +0300

    added File Sharing project

---
+++ BotIntegration.Services.FileSharing/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BotIntegration.Services.FileSharing.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileController(IWebHostEnvironment environment) : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadFile(IFormFile? file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("File is empty");
+        }
+
+        var uploadsFolder = Path.Combine(environment.WebRootPath, "uploads");
+        if (!Directory.Exists(uploadsFolder))
+        {
+            Directory.CreateDirectory(uploadsFolder);
+        }
+
+        var uniqueFileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        await using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(fileStream);
+        }
+
+        var fileUrl = $"{Request.Scheme}://{Request.Host}/uploads/{uniqueFileName}";
+        return Ok(new { fileUrl });
+    }
+}
---
+++ BotIntegration.Services.FileSharing/Program.cs
@@ -0,0 +1,35 @@
+using Serilog;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    
+    builder.Configuration.AddEnvironmentVariables();
+    
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Seq(builder.Configuration["Urls:LogServer"] ?? string.Empty)
+        .CreateLogger();
+
+    builder.Host.UseSerilog((_, configuration) =>
+    {
+        configuration.WriteTo.Console();
+        configuration.WriteTo.Seq(builder.Configuration["Urls:LogServer"] ?? string.Empty);
+    });
+
+    builder.Services.AddControllers();
+
+    var app = builder.Build();
+    app.UseStaticFiles();
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
