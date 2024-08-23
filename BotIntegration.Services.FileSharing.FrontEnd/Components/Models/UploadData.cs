6d8787d34b12cabbc78b0ec37280c1287245bbf9
Nikita Efimov <maskedballmail@gmail.com>
2024-08-23 14:42:15 +0300

    Added file sharing front end project

---
+++ BotIntegration.Services.FileSharing.FrontEnd/Components/Models/UploadData.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BotIntegration.Services.FileSharing.FrontEnd.Components.Models;
+
+public class UploadData
+{
+    [JsonPropertyName("fileUrl")]
+    public string FileUrl { get; set; }
+}
---
+++ BotIntegration.Services.FileSharing.FrontEnd/Program.cs
@@ -0,0 +1,54 @@
+using BotIntegration.Services.FileSharing.FrontEnd.Components;
+using Serilog;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.Limits.MaxRequestBodySize = null;
+    });
+    
+    builder.Configuration.AddEnvironmentVariables();
+    
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Seq(builder.Configuration["Urls:LogServer"] ?? string.Empty)
+        .CreateLogger();
+
+    builder.Host.UseSerilog((_, configuration) =>
+    {
+        configuration.WriteTo.Console();
+        configuration.WriteTo.Seq(builder.Configuration["Urls:LogServer"] ?? string.Empty);
+    });
+    
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+
+    builder.Services.AddHttpClient();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        app.UseHsts();
+    }
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
